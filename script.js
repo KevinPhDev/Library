a7b414e07025fdde5d6472d504500445d8f435ec
KevinPhDev <kevinphdev@outlook.com>
2023-05-04 23:09:54 +1000

    Write for loop to display book titles in text

--- script.js
+++ script.js
@@ -8,5 +8,17 @@ function Book(title, author, pages) {
 
 function addBookToLibrary(title, author, pages) {
     const book = new Book(title, author, pages);
-    myLibrary.push(book);
+    myLibrary.push(book.title);
 }
+
+addBookToLibrary('book1', 'author1', 10);
+addBookToLibrary('book2', 'author2', 20);
+addBookToLibrary('book3', 'author3', 30);
+
+for (i = 0; i < myLibrary.length; i++) {
+    const h1Title = document.createElement('h1');
+    h1Title.innerText = `${myLibrary[i]}`;
+    document.body.appendChild(h1Title);
+}
+
+console.log(myLibrary);
